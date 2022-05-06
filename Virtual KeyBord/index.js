ba7869ca2eb847576a23df222c8513c27f221c19
Arterixs <artemontugg@gmail.com>
2022-05-07 01:17:14 +0300

    init: start task, regenerate html

---
+++ Virtual KeyBord/index.js
@@ -0,0 +1,641 @@
+
+const arrBtn = [
+    {   
+        keyShiftEn: '~',
+        keyShiftRu: "Ё",
+        keyEng: "`",
+        keyRu: "ё",
+        charCode: 0,
+        keyCode: 192,
+        code: "Backquote",
+    },
+    {
+        keyShiftEn: '!',
+        keyShiftRu: "!",
+        keyEng: "1",
+        keyRu: "1",
+        charCode: 0,
+        keyCode: 49,
+        code: "Digit1",
+    },
+    {
+        keyShiftEN: "@",
+        keyShiftRu: '"',
+        keyEng: "2",
+        keyRu: "2",
+        charCode: 0,
+        keyCode: 50,
+        code: "Digit2",
+    },
+    {
+        keyShiftEN: "#",
+        keyShiftRu: '№',
+        keyEng: "3",
+        keyRu: "3",
+        charCode: 0,
+        keyCode: 51,
+        code: "Digit3",
+    },
+    {
+        keyShiftEN: "$",
+        keyShiftRu: ';',
+        keyEng: "4",
+        keyRu: "4",
+        charCode: 0,
+        keyCode: 52,
+        code: "Digit4",
+    },
+    {
+        keyShiftEN: "%",
+        keyShiftRu: '%',
+        keyEng: "5",
+        keyRu: "5",
+        charCode: 0,
+        keyCode: 53,
+        code: "Digit5",
+    },
+    {
+        keyShiftEN: "^",
+        keyShiftRu: ':',
+        keyEng: "6",
+        keyRu: "6",
+        charCode: 0,
+        keyCode: 54,
+        code: "Digit6",
+    },
+    {
+        keyShiftEN: "&",
+        keyShiftRu: '?',
+        keyEng: "7",
+        keyRu: "7",
+        charCode: 0,
+        keyCode: 55,
+        code: "Digit7",
+    },
+    {
+        keyShiftEN: "*",
+        keyShiftRu: '*',
+        keyEng: "8",
+        keyRu: "8",
+        charCode: 0,
+        keyCode: 56,
+        code: "Digit8",
+    },
+    {
+        keyShiftEN: "(",
+        keyShiftRu: '(',
+        keyEng: "9",
+        keyRu: "9",
+        charCode: 0,
+        keyCode: 57,
+        code: "Digit9",
+    },
+    {
+        keyShiftEN: ")",
+        keyShiftRu: ')',
+        keyEng: "0",
+        keyRu: "0",
+        charCode: 0,
+        keyCode: 48,
+        code: "Digit0",
+    },
+    {
+        keyShiftEN: "_",
+        keyShiftRu: '_',
+        keyEng: "-",
+        keyRu: "-",
+        charCode: 0,
+        keyCode: 189,
+        code: "Minus",
+    },
+    {
+        keyShiftEN: "+",
+        keyShiftRu: '+',
+        keyEng: "=",
+        keyRu: "=",
+        charCode: 0,
+        keyCode: 187,
+        code: "Equal",
+    },
+    {
+        keyShiftEN: "+",
+        keyShiftRu: '+',
+        keyEng: "Backspace",
+        keyRu: "Backspace",
+        charCode: 0,
+        keyCode: 8,
+        code: "Backspace",
+    },
+    {
+        keyShiftEN: "",
+        keyShiftRu: '',
+        keyEng: "Tab",
+        keyRu: "Tab",
+        charCode: 0,
+        keyCode: 9,
+        code: "Tab",
+    },
+    {
+        keyShiftEN: "Q",
+        keyShiftRu: 'Й',
+        keyEng: "q",
+        keyRu: "й",
+        charCode: 0,
+        keyCode: 81,
+        code: "KeyQ",
+    },
+    {
+        keyShiftEN: "W",
+        keyShiftRu: 'Ц',
+        keyEng: "w",
+        keyRu: "ц",
+        charCode: 0,
+        keyCode: 87,
+        code: "KeyW",
+    },
+    {
+        keyShiftEN: "E",
+        keyShiftRu: 'У',
+        keyEng: "e",
+        keyRu: "у",
+        charCode: 0,
+        keyCode: 69,
+        code: "KeyE",
+    },
+    {
+        keyShiftEN: "R",
+        keyShiftRu: 'К',
+        keyEng: "r",
+        keyRu: "к",
+        charCode: 0,
+        keyCode: 82,
+        code: "KeyR",
+    },
+    {
+        keyShiftEN: "T",
+        keyShiftRu: 'Е',
+        keyEng: "t",
+        keyRu: "е",
+        charCode: 0,
+        keyCode: 84,
+        code: "KeyT",
+    },
+    {
+        keyShiftEN: "Y",
+        keyShiftRu: 'Н',
+        keyEng: "y",
+        keyRu: "н",
+        charCode: 0,
+        keyCode: 89,
+        code: "KeyY",
+    },
+    {
+        keyShiftEN: "U",
+        keyShiftRu: 'г',
+        keyEng: "u",
+        keyRu: "г",
+        charCode: 0,
+        keyCode: 85,
+        code: "KeyU",
+    },
+    {
+        keyShiftEN: "I",
+        keyShiftRu: 'Ш',
+        keyEng: "i",
+        keyRu: "ш",
+        charCode: 0,
+        keyCode: 73,
+        code: "KeyI",
+    },
+    {
+        keyShiftEN: "O",
+        keyShiftRu: 'Щ',
+        keyEng: "o",
+        keyRu: "щ",
+        charCode: 0,
+        keyCode: 79,
+        code: "KeyO",
+    },
+    {
+        keyShiftEN: "P",
+        keyShiftRu: 'З',
+        keyEng: "p",
+        keyRu: "з",
+        charCode: 0,
+        keyCode: 80,
+        code: "KeyP",
+    },
+    {
+        keyShiftEN: "{",
+        keyShiftRu: 'Х',
+        keyEng: "[",
+        keyRu: "х",
+        charCode: 0,
+        keyCode: 219,
+        code: "BracketLeft",
+    },
+    {
+        keyShiftEN: "}",
+        keyShiftRu: 'Ъ',
+        keyEng: "]",
+        keyRu: "ъ",
+        charCode: 0,
+        keyCode: 221,
+        code: "BracketRight",
+    },
+    {
+        keyShiftEN: "|",
+        keyShiftRu: '/',
+        keyEng: "\\",
+        keyRu: "\\",
+        charCode: 0,
+        keyCode: 220,
+        code: "Backslash",
+    },
+    {
+        keyShiftEN: "",
+        keyShiftRu: '',
+        keyEng: "Del",
+        keyRu: "Del",
+        charCode: 0,
+        keyCode: 220,
+        code: 'Delete',
+    },
+    {
+        keyShiftEN: "",
+        keyShiftRu: '',
+        keyEng: "CapsLock",
+        keyRu: "CapsLock",
+        charCode: 0,
+        keyCode: 20,
+        code: 'CapsLock',
+    },
+    {
+        keyShiftEN: "A",
+        keyShiftRu: 'Ф',
+        keyEng: "a",
+        keyRu: "ф",
+        charCode: 0,
+        keyCode: 65,
+        code: 'KeyA',
+    },
+    {
+        keyShiftEN: "S",
+        keyShiftRu: 'Ы',
+        keyEng: "s",
+        keyRu: "ы",
+        charCode: 0,
+        keyCode: 83,
+        code: 'KeyS',
+    },
+    {
+        keyShiftEN: "D",
+        keyShiftRu: 'В',
+        keyEng: "d",
+        keyRu: "в",
+        charCode: 0,
+        keyCode: 68,
+        code: 'KeyD',
+    },
+    {
+        keyShiftEN: "F",
+        keyShiftRu: 'А',
+        keyEng: "f",
+        keyRu: "а",
+        charCode: 0,
+        keyCode: 70,
+        code: 'KeyF',
+    },
+    {
+        keyShiftEN: "G",
+        keyShiftRu: 'П',
+        keyEng: "g",
+        keyRu: "п",
+        charCode: 0,
+        keyCode: 71,
+        code: 'KeyG',
+    },
+    {
+        keyShiftEN: "H",
+        keyShiftRu: 'Р',
+        keyEng: "h",
+        keyRu: "р",
+        charCode: 0,
+        keyCode: 72,
+        code: 'KeyH',
+    },
+    {
+        keyShiftEN: "J",
+        keyShiftRu: 'О',
+        keyEng: "j",
+        keyRu: "о",
+        charCode: 0,
+        keyCode: 74,
+        code: 'KeyJ',
+    },
+    {
+        keyShiftEN: "K",
+        keyShiftRu: 'Л',
+        keyEng: "k",
+        keyRu: "л",
+        charCode: 0,
+        keyCode: 75,
+        code: 'KeyK',
+    },
+    {
+        keyShiftEN: "L",
+        keyShiftRu: 'Д',
+        keyEng: "l",
+        keyRu: "д",
+        charCode: 0,
+        keyCode: 76,
+        code: 'KeyL',
+    },
+    {
+        keyShiftEN: ":",
+        keyShiftRu: 'Ж',
+        keyEng: ";",
+        keyRu: "ж",
+        charCode: 0,
+        keyCode: 186,
+        code: 'Semicolon',
+    },
+    {
+        keyShiftEN: '"',
+        keyShiftRu: 'Э',
+        keyEng: "'",
+        keyRu: "э",
+        charCode: 0,
+        keyCode: 222,
+        code: 'Quote',
+    },
+    {
+        keyShiftEN: '',
+        keyShiftRu: '',
+        keyEng: "Enter",
+        keyRu: "Enter",
+        charCode: 0,
+        keyCode: 13,
+        code: 'Enter',
+    },
+    {
+        keyShiftEN: 'Shift',
+        keyShiftRu: 'Shift',
+        keyEng: "Shift",
+        keyRu: "Shift",
+        charCode: 0,
+        keyCode: 16,
+        code: 'ShiftLeft',
+    },
+    {
+        keyShiftEN: 'Z',
+        keyShiftRu: 'Я',
+        keyEng: "z",
+        keyRu: "я",
+        charCode: 0,
+        keyCode: 90,
+        code: 'KeyZ',
+    },
+    {
+        keyShiftEN: 'X',
+        keyShiftRu: 'Ч',
+        keyEng: "x",
+        keyRu: "ч",
+        charCode: 0,
+        keyCode: 88,
+        code: 'KeyX',
+    },
+    {
+        keyShiftEN: 'C',
+        keyShiftRu: 'c',
+        keyEng: "c",
+        keyRu: "с",
+        charCode: 0,
+        keyCode: 67,
+        code: 'KeyC',
+    },
+    {
+        keyShiftEN: 'V',
+        keyShiftRu: 'м',
+        keyEng: "v",
+        keyRu: "м",
+        charCode: 0,
+        keyCode: 86,
+        code: 'KeyV',
+    },
+    {
+        keyShiftEN: 'B',
+        keyShiftRu: 'И',
+        keyEng: "b",
+        keyRu: "и",
+        charCode: 0,
+        keyCode: 66,
+        code: 'KeyB',
+    },
+    {
+        keyShiftEN: 'N',
+        keyShiftRu: 'Т',
+        keyEng: "n",
+        keyRu: "т",
+        charCode: 0,
+        keyCode: 78,
+        code: 'KeyN',
+    },
+    {
+        keyShiftEN: 'M',
+        keyShiftRu: 'Ь',
+        keyEng: "m",
+        keyRu: "ь",
+        charCode: 0,
+        keyCode: 77,
+        code: 'KeyM',
+    },
+    {
+        keyShiftEN: '<',
+        keyShiftRu: 'Б',
+        keyEng: ",",
+        keyRu: "б",
+        charCode: 0,
+        keyCode: 188,
+        code: 'Comma',
+    },
+    {
+        keyShiftEN: '>',
+        keyShiftRu: 'Ю',
+        keyEng: ".",
+        keyRu: "Ю",
+        charCode: 0,
+        keyCode: 190,
+        code: 'Period',
+    },
+    {
+        keyShiftEN: '?',
+        keyShiftRu: ',',
+        keyEng: "/",
+        keyRu: ".",
+        charCode: 0,
+        keyCode: 191,
+        code: 'Slash',
+    },
+    {
+        keyShiftEN: 'up',
+        keyShiftRu: 'up',
+        keyEng: "up",
+        keyRu: "up",
+        charCode: 0,
+        keyCode: 38,
+        code: 'ArrowUp',
+    },
+    {
+        keyShiftEN: 'Shift',
+        keyShiftRu: 'Shift',
+        keyEng: "Shift",
+        keyRu: "Shift",
+        charCode: 0,
+        keyCode: 16,
+        code: 'ShiftRight',
+    },
+    {
+        keyShiftEN: 'Ctrl',
+        keyShiftRu: 'Ctrl',
+        keyEng: "Ctrl",
+        keyRu: "Ctrl",
+        charCode: 0,
+        keyCode: 17,
+        code: 'ControlLeft',
+    },
+    {
+        keyShiftEN: 'Win',
+        keyShiftRu: 'Win',
+        keyEng: "Win",
+        keyRu: "Win",
+        charCode: 0,
+        keyCode: 255,
+        code: 'WakeUp',
+    },
+    {
+        keyShiftEN: 'Alt',
+        keyShiftRu: 'Alt',
+        keyEng: "Alt",
+        keyRu: "Alt",
+        charCode: 0,
+        keyCode: 18,
+        code: 'AltLeft',
+    },
+    {
+        keyShiftEN: '',
+        keyShiftRu: '',
+        keyEng: "",
+        keyRu: "",
+        charCode: 0,
+        keyCode: 32,
+        code: 'Space',
+    },
+    {
+        keyShiftEN: 'Alt',
+        keyShiftRu: 'Alt',
+        keyEng: "Alt",
+        keyRu: "Alt",
+        charCode: 0,
+        keyCode: 18,
+        code: 'AltRight',
+    },
+    {
+        keyShiftEN: 'left',
+        keyShiftRu: 'left',
+        keyEng: "left",
+        keyRu: "left",
+        charCode: 0,
+        keyCode: 37,
+        code: 'ArrowLeft',
+    },
+    {
+        keyShiftEN: 'down',
+        keyShiftRu: 'down',
+        keyEng: "down",
+        keyRu: "down",
+        charCode: 0,
+        keyCode: 40,
+        code: 'ArrowDown',
+    },
+    {
+        keyShiftEN: 'right',
+        keyShiftRu: 'right',
+        keyEng: "right",
+        keyRu: "right",
+        charCode: 0,
+        keyCode: 39,
+        code: 'ArrowRight',
+    },
+    {
+        keyShiftEN: 'Ctrl',
+        keyShiftRu: 'Ctrl',
+        keyEng: "Ctrl",
+        keyRu: "Ctrl",
+        charCode: 0,
+        keyCode: 17,
+        code: 'ControlRight',
+    },
+]
+
+
+class Regen {
+    constructor(nameElement) {
+        this.nameElement = nameElement
+        
+    }
+    createElemBody(parent, classEl) {
+        const div = document.createElement(this.nameElement)
+        div.className = classEl;
+        parent.append(div);
+        return this
+    }
+    creatText(parent, classEl, text) {
+        const str = document.createElement(this.nameElement)
+        str.className = classEl;
+        str.innerHTML = text;
+        parent.prepend(str)
+    }
+    createButton(parent, classEl) {
+        for (let i = 0; i < 64; i++) {
+            const btn = document.createElement(this.nameElement)
+            const p = document.createElement("p")
+            btn.className = classEl;
+            p.className = "btnName"
+            p.innerHTML = arrBtn[i].keyEng
+            parent.append(btn)
+            btn.append(p)
+            i === 13 ?  btn.className = classEl + " backspace" : 0
+            i === 14 ?  btn.className = classEl + " tab" : 0
+            i === 28 ?  btn.className = classEl + " delete" : 0
+            i === 29 ?  btn.className = classEl + " capsLock" : 0
+            i === 41 ?  btn.className = classEl + " enter" : 0
+            i === 42 ?  btn.className = classEl + " shift" : 0
+            i === 54 ?  btn.className = classEl + " shift" : 0
+            i === 58 ?  btn.className = classEl + " space" : 0
+        }
+    }
+}
+
+const regen = new Regen("div")
+const textarea = new Regen("textarea")
+const h1 = new Regen("h1")
+const p = new Regen("p")
+
+
+regen.createElemBody(document.body, "wrapper")
+h1.creatText(document.body.childNodes[2], "title_key", "Virtual Keyboard")
+regen.createElemBody(document.body.childNodes[2], "container")
+regen.createElemBody(document.body.childNodes[2].lastChild, "wrapper_monitor")
+textarea.createElemBody(document.body.childNodes[2].lastChild.firstChild, "monitor")
+regen.createElemBody(document.body.childNodes[2].lastChild, "wrapper_keyboard")
+regen.createElemBody(document.body.childNodes[2].lastChild.lastChild, "line")
+regen.createButton(document.body.childNodes[2].lastChild.lastChild.firstChild, "button")
+
+
+
+document.addEventListener("keydown", function(KeyboardEvent) {
+    console.log(KeyboardEvent)
+})
+
+
