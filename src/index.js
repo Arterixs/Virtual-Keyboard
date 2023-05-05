61335b0b496f00b5735c5bbbd5859f253c4ca4be
Arterixs <artemontugg@gmail.com>
2023-05-05 16:01:35 +0300

    refactor: took out an array of data btn from the main file

--- src/index.js
+++ src/index.js
@@ -1,645 +1,4 @@
-const arrBtn = [
-  {
-    keyShiftEN: "~",
-    keyShiftRu: "Ё",
-    keyCapsRu: "Ё",
-    keyEng: "`",
-    keyRu: "ё",
-    charCode: 0,
-    keyCode: 192,
-    code: "Backquote",
-  },
-  {
-    keyShiftEN: "!",
-    keyShiftRu: "!",
-    keyCapsRu: "1",
-    keyEng: "1",
-    keyRu: "1",
-    charCode: 0,
-    keyCode: 49,
-    code: "Digit1",
-  },
-  {
-    keyShiftEN: "@",
-    keyShiftRu: '"',
-    keyCapsRu: "2",
-    keyEng: "2",
-    keyRu: "2",
-    charCode: 0,
-    keyCode: 50,
-    code: "Digit2",
-  },
-  {
-    keyShiftEN: "#",
-    keyShiftRu: "№",
-    keyCapsRu: "3",
-    keyEng: "3",
-    keyRu: "3",
-    charCode: 0,
-    keyCode: 51,
-    code: "Digit3",
-  },
-  {
-    keyShiftEN: "$",
-    keyShiftRu: ";",
-    keyCapsRu: "4",
-    keyEng: "4",
-    keyRu: "4",
-    charCode: 0,
-    keyCode: 52,
-    code: "Digit4",
-  },
-  {
-    keyShiftEN: "%",
-    keyShiftRu: "%",
-    keyCapsRu: "5",
-    keyEng: "5",
-    keyRu: "5",
-    charCode: 0,
-    keyCode: 53,
-    code: "Digit5",
-  },
-  {
-    keyShiftEN: "^",
-    keyShiftRu: ":",
-    keyCapsRu: "6",
-    keyEng: "6",
-    keyRu: "6",
-    charCode: 0,
-    keyCode: 54,
-    code: "Digit6",
-  },
-  {
-    keyShiftEN: "&",
-    keyShiftRu: "?",
-    keyCapsRu: "7",
-    keyEng: "7",
-    keyRu: "7",
-    charCode: 0,
-    keyCode: 55,
-    code: "Digit7",
-  },
-  {
-    keyShiftEN: "*",
-    keyShiftRu: "*",
-    keyCapsRu: "8",
-    keyEng: "8",
-    keyRu: "8",
-    charCode: 0,
-    keyCode: 56,
-    code: "Digit8",
-  },
-  {
-    keyShiftEN: "(",
-    keyShiftRu: "(",
-    keyCapsRu: "9",
-    keyEng: "9",
-    keyRu: "9",
-    charCode: 0,
-    keyCode: 57,
-    code: "Digit9",
-  },
-  {
-    keyShiftEN: ")",
-    keyShiftRu: ")",
-    keyCapsRu: "0",
-    keyEng: "0",
-    keyRu: "0",
-    charCode: 0,
-    keyCode: 48,
-    code: "Digit0",
-  },
-  {
-    keyShiftEN: "_",
-    keyShiftRu: "_",
-    keyCapsRu: "-",
-    keyEng: "-",
-    keyRu: "-",
-    charCode: 0,
-    keyCode: 189,
-    code: "Minus",
-  },
-  {
-    keyShiftEN: "+",
-    keyShiftRu: "+",
-    keyCapsRu: "=",
-    keyEng: "=",
-    keyRu: "=",
-    charCode: 0,
-    keyCode: 187,
-    code: "Equal",
-  },
-  {
-    keyShiftEN: "Backspace",
-    keyShiftRu: "Backspace",
-    keyCapsRu: "Backspace",
-    keyEng: "Backspace",
-    keyRu: "Backspace",
-    charCode: 0,
-    keyCode: 8,
-    code: "Backspace",
-  },
-  {
-    keyShiftEN: "Tab",
-    keyShiftRu: "Tab",
-    keyCapsRu: "Tab",
-    keyEng: "Tab",
-    keyRu: "Tab",
-    charCode: 0,
-    keyCode: 9,
-    code: "Tab",
-  },
-  {
-    keyShiftEN: "Q",
-    keyShiftRu: "Й",
-    keyCapsRu: "Й",
-    keyEng: "q",
-    keyRu: "й",
-    charCode: 0,
-    keyCode: 81,
-    code: "KeyQ",
-  },
-  {
-    keyShiftEN: "W",
-    keyShiftRu: "Ц",
-    keyCapsRu: "Ц",
-    keyEng: "w",
-    keyRu: "ц",
-    charCode: 0,
-    keyCode: 87,
-    code: "KeyW",
-  },
-  {
-    keyShiftEN: "E",
-    keyShiftRu: "У",
-    keyCapsRu: "У",
-    keyEng: "e",
-    keyRu: "у",
-    charCode: 0,
-    keyCode: 69,
-    code: "KeyE",
-  },
-  {
-    keyShiftEN: "R",
-    keyShiftRu: "К",
-    keyCapsRu: "К",
-    keyEng: "r",
-    keyRu: "к",
-    charCode: 0,
-    keyCode: 82,
-    code: "KeyR",
-  },
-  {
-    keyShiftEN: "T",
-    keyShiftRu: "Е",
-    keyCapsRu: "Е",
-    keyEng: "t",
-    keyRu: "е",
-    charCode: 0,
-    keyCode: 84,
-    code: "KeyT",
-  },
-  {
-    keyShiftEN: "Y",
-    keyShiftRu: "Н",
-    keyCapsRu: "Н",
-    keyEng: "y",
-    keyRu: "н",
-    charCode: 0,
-    keyCode: 89,
-    code: "KeyY",
-  },
-  {
-    keyShiftEN: "U",
-    keyShiftRu: "Г",
-    keyCapsRu: "Г",
-    keyEng: "u",
-    keyRu: "г",
-    charCode: 0,
-    keyCode: 85,
-    code: "KeyU",
-  },
-  {
-    keyShiftEN: "I",
-    keyShiftRu: "Ш",
-    keyCapsRu: "Ш",
-    keyEng: "i",
-    keyRu: "ш",
-    charCode: 0,
-    keyCode: 73,
-    code: "KeyI",
-  },
-  {
-    keyShiftEN: "O",
-    keyShiftRu: "Щ",
-    keyCapsRu: "Щ",
-    keyEng: "o",
-    keyRu: "щ",
-    charCode: 0,
-    keyCode: 79,
-    code: "KeyO",
-  },
-  {
-    keyShiftEN: "P",
-    keyShiftRu: "З",
-    keyCapsRu: "З",
-    keyEng: "p",
-    keyRu: "з",
-    charCode: 0,
-    keyCode: 80,
-    code: "KeyP",
-  },
-  {
-    keyShiftEN: "{",
-    keyShiftRu: "Х",
-    keyCapsRu: "Х",
-    keyEng: "[",
-    keyRu: "х",
-    charCode: 0,
-    keyCode: 219,
-    code: "BracketLeft",
-  },
-  {
-    keyShiftEN: "}",
-    keyShiftRu: "Ъ",
-    keyCapsRu: "Ъ",
-    keyEng: "]",
-    keyRu: "ъ",
-    charCode: 0,
-    keyCode: 221,
-    code: "BracketRight",
-  },
-  {
-    keyShiftEN: "|",
-    keyShiftRu: "/",
-    keyCapsRu: "\\",
-    keyEng: "\\",
-    keyRu: "\\",
-    charCode: 0,
-    keyCode: 220,
-    code: "Backslash",
-  },
-  {
-    keyShiftEN: "Del",
-    keyShiftRu: "Del",
-    keyCapsRu: "Del",
-    keyEng: "Del",
-    keyRu: "Del",
-    charCode: 0,
-    keyCode: 46,
-    code: "Delete",
-  },
-  {
-    keyShiftEN: "CapsLock",
-    keyShiftRu: "CapsLock",
-    keyCapsRu: "CapsLock",
-    keyEng: "CapsLock",
-    keyRu: "CapsLock",
-    charCode: 0,
-    keyCode: 20,
-    code: "CapsLock",
-  },
-  {
-    keyShiftEN: "A",
-    keyShiftRu: "Ф",
-    keyCapsRu: "Ф",
-    keyEng: "a",
-    keyRu: "ф",
-    charCode: 0,
-    keyCode: 65,
-    code: "KeyA",
-  },
-  {
-    keyShiftEN: "S",
-    keyShiftRu: "Ы",
-    keyCapsRu: "Ы",
-    keyEng: "s",
-    keyRu: "ы",
-    charCode: 0,
-    keyCode: 83,
-    code: "KeyS",
-  },
-  {
-    keyShiftEN: "D",
-    keyShiftRu: "В",
-    keyCapsRu: "В",
-    keyEng: "d",
-    keyRu: "в",
-    charCode: 0,
-    keyCode: 68,
-    code: "KeyD",
-  },
-  {
-    keyShiftEN: "F",
-    keyShiftRu: "А",
-    keyCapsRu: "А",
-    keyEng: "f",
-    keyRu: "а",
-    charCode: 0,
-    keyCode: 70,
-    code: "KeyF",
-  },
-  {
-    keyShiftEN: "G",
-    keyShiftRu: "П",
-    keyCapsRu: "П",
-    keyEng: "g",
-    keyRu: "п",
-    charCode: 0,
-    keyCode: 71,
-    code: "KeyG",
-  },
-  {
-    keyShiftEN: "H",
-    keyShiftRu: "Р",
-    keyCapsRu: "Р",
-    keyEng: "h",
-    keyRu: "р",
-    charCode: 0,
-    keyCode: 72,
-    code: "KeyH",
-  },
-  {
-    keyShiftEN: "J",
-    keyShiftRu: "О",
-    keyCapsRu: "О",
-    keyEng: "j",
-    keyRu: "о",
-    charCode: 0,
-    keyCode: 74,
-    code: "KeyJ",
-  },
-  {
-    keyShiftEN: "K",
-    keyShiftRu: "Л",
-    keyCapsRu: "Л",
-    keyEng: "k",
-    keyRu: "л",
-    charCode: 0,
-    keyCode: 75,
-    code: "KeyK",
-  },
-  {
-    keyShiftEN: "L",
-    keyShiftRu: "Д",
-    keyCapsRu: "Д",
-    keyEng: "l",
-    keyRu: "д",
-    charCode: 0,
-    keyCode: 76,
-    code: "KeyL",
-  },
-  {
-    keyShiftEN: ":",
-    keyShiftRu: "Ж",
-    keyCapsRu: "Ж",
-    keyEng: ";",
-    keyRu: "ж",
-    charCode: 0,
-    keyCode: 186,
-    code: "Semicolon",
-  },
-  {
-    keyShiftEN: '"',
-    keyShiftRu: "Э",
-    keyCapsRu: "Э",
-    keyEng: "'",
-    keyRu: "э",
-    charCode: 0,
-    keyCode: 222,
-    code: "Quote",
-  },
-  {
-    keyShiftEN: "Enter",
-    keyShiftRu: "Enter",
-    keyCapsRu: "Enter",
-    keyEng: "Enter",
-    keyRu: "Enter",
-    charCode: 0,
-    keyCode: 13,
-    code: "Enter",
-  },
-  {
-    keyShiftEN: "Shift",
-    keyShiftRu: "Shift",
-    keyCapsRu: "Shift",
-    keyEng: "Shift",
-    keyRu: "Shift",
-    charCode: 0,
-    keyCode: 16,
-    code: "ShiftLeft",
-  },
-  {
-    keyShiftEN: "Z",
-    keyShiftRu: "Я",
-    keyCapsRu: "Я",
-    keyEng: "z",
-    keyRu: "я",
-    charCode: 0,
-    keyCode: 90,
-    code: "KeyZ",
-  },
-  {
-    keyShiftEN: "X",
-    keyShiftRu: "Ч",
-    keyCapsRu: "Ч",
-    keyEng: "x",
-    keyRu: "ч",
-    charCode: 0,
-    keyCode: 88,
-    code: "KeyX",
-  },
-  {
-    keyShiftEN: "C",
-    keyShiftRu: "С",
-    keyCapsRu: "С",
-    keyEng: "c",
-    keyRu: "с",
-    charCode: 0,
-    keyCode: 67,
-    code: "KeyC",
-  },
-  {
-    keyShiftEN: "V",
-    keyShiftRu: "М",
-    keyCapsRu: "М",
-    keyEng: "v",
-    keyRu: "м",
-    charCode: 0,
-    keyCode: 86,
-    code: "KeyV",
-  },
-  {
-    keyShiftEN: "B",
-    keyShiftRu: "И",
-    keyCapsRu: "И",
-    keyEng: "b",
-    keyRu: "и",
-    charCode: 0,
-    keyCode: 66,
-    code: "KeyB",
-  },
-  {
-    keyShiftEN: "N",
-    keyShiftRu: "Т",
-    keyCapsRu: "Т",
-    keyEng: "n",
-    keyRu: "т",
-    charCode: 0,
-    keyCode: 78,
-    code: "KeyN",
-  },
-  {
-    keyShiftEN: "M",
-    keyShiftRu: "Ь",
-    keyCapsRu: "Ь",
-    keyEng: "m",
-    keyRu: "ь",
-    charCode: 0,
-    keyCode: 77,
-    code: "KeyM",
-  },
-  {
-    keyShiftEN: "<",
-    keyShiftRu: "Б",
-    keyCapsRu: "Б",
-    keyEng: ",",
-    keyRu: "б",
-    charCode: 0,
-    keyCode: 188,
-    code: "Comma",
-  },
-  {
-    keyShiftEN: ">",
-    keyShiftRu: "Ю",
-    keyCapsRu: "Ю",
-    keyEng: ".",
-    keyRu: "ю",
-    charCode: 0,
-    keyCode: 190,
-    code: "Period",
-  },
-  {
-    keyShiftEN: "?",
-    keyShiftRu: ",",
-    keyCapsRu: ".",
-    keyEng: "/",
-    keyRu: ".",
-    charCode: 0,
-    keyCode: 191,
-    code: "Slash",
-  },
-  {
-    keyShiftEN: "⇧",
-    keyShiftRu: "⇧",
-    keyCapsRu: "⇧",
-    keyEng: "⇧",
-    keyRu: "⇧",
-    charCode: 0,
-    keyCode: 38,
-    code: "ArrowUp",
-  },
-  {
-    keyShiftEN: "Shift",
-    keyShiftRu: "Shift",
-    keyCapsRu: "Shift",
-    keyEng: "Shift",
-    keyRu: "Shift",
-    charCode: 0,
-    keyCode: 16,
-    code: "ShiftRight",
-  },
-  {
-    keyShiftEN: "Ctrl",
-    keyShiftRu: "Ctrl",
-    keyCapsRu: "Ctrl",
-    keyEng: "Ctrl",
-    keyRu: "Ctrl",
-    charCode: 0,
-    keyCode: 17,
-    code: "ControlLeft",
-  },
-  {
-    keyShiftEN: "Win",
-    keyShiftRu: "Win",
-    keyCapsRu: "Win",
-    keyEng: "Win",
-    keyRu: "Win",
-    charCode: 0,
-    keyCode: 255,
-    code: "WakeUp",
-  },
-  {
-    keyShiftEN: "Alt",
-    keyShiftRu: "Alt",
-    keyCapsRu: "Alt",
-    keyEng: "Alt",
-    keyRu: "Alt",
-    charCode: 0,
-    keyCode: 18,
-    code: "AltLeft",
-  },
-  {
-    keyShiftEN: "",
-    keyShiftRu: "",
-    keyCapsRu: " ",
-    keyEng: " ",
-    keyRu: " ",
-    charCode: 0,
-    keyCode: 32,
-    code: "Space",
-  },
-  {
-    keyShiftEN: "Alt",
-    keyShiftRu: "Alt",
-    keyCapsRu: "Alt",
-    keyEng: "Alt",
-    keyRu: "Alt",
-    charCode: 0,
-    keyCode: 18,
-    code: "AltRight",
-  },
-  {
-    keyShiftEN: "⇦",
-    keyShiftRu: "⇦",
-    keyCapsRu: "⇦",
-    keyEng: "⇦",
-    keyRu: "⇦",
-    charCode: 0,
-    keyCode: 37,
-    code: "ArrowLeft",
-  },
-  {
-    keyShiftEN: "⇩",
-    keyShiftRu: "⇩",
-    keyCapsRu: "⇩",
-    keyEng: "⇩",
-    keyRu: "⇩",
-    charCode: 0,
-    keyCode: 40,
-    code: "ArrowDown",
-  },
-  {
-    keyShiftEN: "⇨",
-    keyShiftRu: "⇨",
-    keyCapsRu: "⇨",
-    keyEng: "⇨",
-    keyRu: "⇨",
-    charCode: 0,
-    keyCode: 39,
-    code: "ArrowRight",
-  },
-  {
-    keyShiftEN: "Ctrl",
-    keyShiftRu: "Ctrl",
-    keyCapsRu: "Ctrl",
-    keyEng: "Ctrl",
-    keyRu: "Ctrl",
-    charCode: 0,
-    keyCode: 17,
-    code: "ControlRight",
-  },
-];
+import { arrButtonData } from "./data/data-array-btn.js";
 
 class Regen {
   constructor(nameElement) {
@@ -672,11 +31,11 @@ class Regen {
       const p = document.createElement("p");
 
       btn.className = classEl;
-      btn.setAttribute("id", arrBtn[i].code);
+      btn.setAttribute("id", arrButtonData[i].code);
       btn.onclick = inputValue;
       p.className = "btnName";
-      p.setAttribute("id", arrBtn[i].keyCode);
-      p.innerHTML = arrBtn[i].keyEng;
+      p.setAttribute("id", arrButtonData[i].keyCode);
+      p.innerHTML = arrButtonData[i].keyEng;
       parent.append(btn);
       btn.append(p);
       if (i === 13) {
@@ -726,17 +85,17 @@ const h1 = new Regen("h1");
 const but = new Regen("button");
 
 regen.createElemBody(document.body, "wrapper");
-h1.creatText(document.body.childNodes[2], "title_key", "Virtual Keyboard");
-regen.createElemBody(document.body.childNodes[2], "container");
-regen.createElemBody(document.body.childNodes[2].lastChild, "wrapper_monitor");
+h1.creatText(document.body.childNodes[7], "title_key", "Virtual Keyboard");
+regen.createElemBody(document.body.childNodes[7], "container");
+regen.createElemBody(document.body.childNodes[7].lastChild, "wrapper_monitor");
 textarea.createElemBody(
-  document.body.childNodes[2].lastChild.firstChild,
+  document.body.childNodes[7].lastChild.firstChild,
   "monitor"
 );
-regen.createElemBody(document.body.childNodes[2].lastChild, "wrapper_keyboard");
-regen.createElemBody(document.body.childNodes[2].lastChild.lastChild, "line");
+regen.createElemBody(document.body.childNodes[7].lastChild, "wrapper_keyboard");
+regen.createElemBody(document.body.childNodes[7].lastChild.lastChild, "line");
 but.createButton(
-  document.body.childNodes[2].lastChild.lastChild.firstChild,
+  document.body.childNodes[7].lastChild.lastChild.firstChild,
   "button"
 );
 
@@ -865,12 +224,12 @@ function logicEnter(event) {
 
 function logicShift() {
   if (localStorage.getItem("language") === "en") {
-    arrBtn.forEach((item) => {
+    arrButtonData.forEach((item) => {
       const valueBut = document.getElementById(`${item.keyCode}`);
       valueBut.textContent = item.keyShiftEN;
     });
   } else {
-    arrBtn.forEach((item) => {
+    arrButtonData.forEach((item) => {
       const valueBut = document.getElementById(`${item.keyCode}`);
       valueBut.textContent = item.keyShiftRu;
     });
@@ -879,12 +238,12 @@ function logicShift() {
 
 function shiftMouseUp() {
   if (localStorage.getItem("language") === "en") {
-    arrBtn.forEach((item) => {
+    arrButtonData.forEach((item) => {
       const valueBut = document.getElementById(`${item.keyCode}`);
       valueBut.textContent = item.keyEng;
     });
   } else {
-    arrBtn.forEach((item) => {
+    arrButtonData.forEach((item) => {
       const valueBut = document.getElementById(`${item.keyCode}`);
       valueBut.textContent = item.keyRu;
     });
@@ -894,7 +253,7 @@ function shiftMouseUp() {
 function logicCaps() {
   if (localStorage.getItem("language") === "en") {
     if (localStorage.getItem("caps") !== "off") {
-      arrBtn.forEach((item) => {
+      arrButtonData.forEach((item) => {
         if (item.code === "Key" + `${item.keyShiftEN}`) {
           const valueBut = document.getElementById(`${item.keyCode}`);
           valueBut.textContent = item.keyEng;
@@ -902,14 +261,14 @@ function logicCaps() {
       });
       return localStorage.setItem("caps", "off");
     }
-    arrBtn.forEach((item) => {
+    arrButtonData.forEach((item) => {
       const valueBut = document.getElementById(`${item.keyCode}`);
       valueBut.textContent = item.keyShiftEN;
     });
     return localStorage.removeItem("caps");
   }
   if (localStorage.getItem("caps") !== "off") {
-    arrBtn.forEach((item) => {
+    arrButtonData.forEach((item) => {
       if (item.code === `${item.code}`) {
         const valueBut = document.getElementById(`${item.keyCode}`);
         valueBut.textContent = item.keyRu;
@@ -917,7 +276,7 @@ function logicCaps() {
     });
     return localStorage.setItem("caps", "off");
   }
-  arrBtn.forEach((item) => {
+  arrButtonData.forEach((item) => {
     const valueBut = document.getElementById(`${item.keyCode}`);
     valueBut.textContent = item.keyCapsRu;
   });
@@ -927,7 +286,7 @@ function logicCaps() {
 function inputValue() {
   const res = this.id;
   const idText = document.getElementById("textarea");
-  arrBtn.forEach((item) => {
+  arrButtonData.forEach((item) => {
     if (
       res === item.code &&
       res !== "ShiftLeft" &&
@@ -971,7 +330,7 @@ document.addEventListener("keydown", (KeyboardEvent) => {
     KeyboardEvent.keyCode === 38
   ) {
     KeyboardEvent.preventDefault();
-    arrBtn.forEach((item) => {
+    arrButtonData.forEach((item) => {
       if (item.code === KeyboardEvent.code) {
         idText.value += document.getElementById(`${item.code}`).textContent;
       }
@@ -980,7 +339,7 @@ document.addEventListener("keydown", (KeyboardEvent) => {
   if (KeyboardEvent.keyCode === 18) {
     KeyboardEvent.preventDefault();
   }
-  arrBtn.forEach((item) => {
+  arrButtonData.forEach((item) => {
     if (KeyboardEvent.code === item.code) {
       document.getElementById(`${item.code}`).classList.add("active");
     }
@@ -989,7 +348,7 @@ document.addEventListener("keydown", (KeyboardEvent) => {
 });
 
 document.addEventListener("keyup", (KeyboardEvent) => {
-  arrBtn.forEach((item) => {
+  arrButtonData.forEach((item) => {
     if (KeyboardEvent.code === item.code) {
       document.getElementById(`${item.code}`).classList.remove("active");
     }
@@ -1001,13 +360,13 @@ document.addEventListener("keyup", (KeyboardEvent) => {
 
 function layoutEn() {
   if (localStorage.getItem("language") === "en") {
-    arrBtn.forEach((item) => {
+    arrButtonData.forEach((item) => {
       const valueBut = document.getElementById(`${item.keyCode}`);
       valueBut.textContent = item.keyRu;
     });
     return localStorage.removeItem("language");
   }
-  arrBtn.forEach((item) => {
+  arrButtonData.forEach((item) => {
     const valueBut = document.getElementById(`${item.keyCode}`);
     valueBut.textContent = item.keyEng;
   });
